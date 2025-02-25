267218a2ab21f5e8aabb6595ec00fc1e91c71710
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-02-24 21:48:07 -0300

    Adicionando Inserir Usuário

--- conexaoBanco/app/src/main/java/conexaoBanco/InserirUsuario.java
+++ conexaoBanco/app/src/main/java/conexaoBanco/InserirUsuario.java
@@ -1,4 +1,21 @@
 package conexaoBanco;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class InserirUsuario {
+	public static void inserirUsuario(Connection conexao, String nome, String email) {
+		String sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+		
+		try ( PreparedStatement pstmt = conexao.prepareStatement(sql)) {
+			pstmt.setString(1, nome);
+			pstmt.setString(2, email);
+			
+			pstmt.executeUpdate(sql);
+			
+			System.out.println("Usuário Inserido com sucesso");
+		} catch (Exception e) {
+			System.out.println("Erro ao inserir usuário" + e.getMessage());
+		}
+	}
 }
